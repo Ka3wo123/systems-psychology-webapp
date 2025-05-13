bf1f470b96e874bc4d2c748d0978d18f3715e0c3
Kajetan Nowak <kajtek2n@gmail.com>
2025-05-13 13:37:47 +0200

    Init push

---
+++ src/app.tsx
@@ -0,0 +1,10 @@
+import "./app.css";
+import Bio from "./components/Bio";
+
+export function App() {
+  return (
+    <>
+      <Bio />
+    </>
+  );
+}
---
+++ src/components/Bio.tsx
@@ -0,0 +1,9 @@
+const Bio = () => {
+  return (
+    <>
+      <p>Hello</p>
+    </>
+  );
+};
+
+export default Bio;
