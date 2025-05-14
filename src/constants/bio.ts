acd5c73da7a6eae47045d71013ffb1c4615b66f8
Kajetan Nowak <kajtek2n@gmail.com>
2025-05-14 14:21:58 +0200

    Overall structure of page

--- src/constants/bio.ts
+++ src/constants/bio.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export const DESCRIPTION = `
   What is Lorem Ipsum?
   Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
@@ -6,10 +5,3 @@ export const DESCRIPTION = `
 
 export const SYSTEM_PSYCHOLOGY = "Psychologia systemowa";
 export const WORKER_NAME = "dr Beata Zych";
-=======
-export const DESCRIPTION = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-  tempor incididunt ut labore et dolore magna aliqua..`;
-
-export const SYSTEM_PSYCHOLOGY = "Psychologia systemowa";
-export const OWNER_NAME = "dr Beata Zych";
->>>>>>> a419378 (Overall structure of page)
--- src/constants/services.ts
+++ src/constants/services.ts
@@ -4,11 +4,8 @@ export const SERVICES = [
     price: "150 zł",
     description:
       "Indywidualne spotkanie mające na celu omówienie trudności dziecka.",
-<<<<<<< HEAD
     additionalText:
       "Przykład tooltip jeśli wymaga większego opisu. Np. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-=======
->>>>>>> a419378 (Overall structure of page)
   },
   {
     name: "Diagnoza psychologiczna",
@@ -21,7 +18,6 @@ export const SERVICES = [
     description:
       "Regularne sesje terapeutyczne dostosowane do potrzeb dziecka.",
   },
-<<<<<<< HEAD
   {
     name: "Terapia indywidualna",
     price: "180 zł",
@@ -40,8 +36,6 @@ export const SERVICES = [
     description:
       "Regularne sesje terapeutyczne dostosowane do potrzeb dziecka.",
   },
-=======
->>>>>>> a419378 (Overall structure of page)
 ];
 
 export const SERVICE_HEADER = "Cennik usług";
