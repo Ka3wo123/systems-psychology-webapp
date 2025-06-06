cf798d0c1e81e31a62e87bf19107e6ea4f8f80f3
Kajetan Nowak <kajtek2n@gmail.com>
2025-06-06 15:16:35 +0200

    Resolve conflicts

--- src/constants/footer.ts
+++ src/constants/footer.ts
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 export const PAGE_COPYRIGHT = `© 2025 dr Beata Zych`;
+=======
+export const PAGE_COPYRIGHT = `© ${new Date().getFullYear()}  dr Beata Zych`;
+>>>>>>> a419378 (Overall structure of page)
