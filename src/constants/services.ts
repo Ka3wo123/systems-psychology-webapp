5077a3f1e95f8b5f703f9fae7e07e3da2cf3529b
Kajetan Nowak <kajtek2n@gmail.com>
2025-06-06 15:16:35 +0200

    Resolve conflicts

--- src/constants/services.ts
+++ src/constants/services.ts
@@ -4,8 +4,11 @@ export const SERVICES = [
     price: "150 zł",
     description:
       "Indywidualne spotkanie mające na celu omówienie trudności dziecka.",
+<<<<<<< HEAD
     additionalText:
       "Przykład tooltip jeśli wymaga większego opisu. Np. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+=======
+>>>>>>> a419378 (Overall structure of page)
   },
   {
     name: "Diagnoza psychologiczna",
@@ -18,6 +21,7 @@ export const SERVICES = [
     description:
       "Regularne sesje terapeutyczne dostosowane do potrzeb dziecka.",
   },
+<<<<<<< HEAD
   {
     name: "Terapia indywidualna",
     price: "180 zł",
@@ -36,6 +40,8 @@ export const SERVICES = [
     description:
       "Regularne sesje terapeutyczne dostosowane do potrzeb dziecka 2.",
   },
+=======
+>>>>>>> a419378 (Overall structure of page)
 ];
 
 export const SERVICE_HEADER = "Cennik usług";
