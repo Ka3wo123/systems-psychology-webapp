647346ebb8020c11be71ced310cbce77e39137f6
Kajetan Nowak <kajtek2n@gmail.com>
2025-06-06 15:16:35 +0200

    Resolve conflicts

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,6 +5,7 @@ import { useState } from "preact/hooks";
 export default function Nav() {
   const [isOpen, setIsOpen] = useState(false);
   const toggleMenu = () => setIsOpen(!isOpen);
+<<<<<<< HEAD
 
   return (
     <nav class="w-fit px-4 md:px-8 lg:px-12 absolute top-10 right-0 z-50">
@@ -12,6 +13,21 @@ export default function Nav() {
         <ul class="hidden md:flex gap-6 text-lg font-medium text-white items-center">
           {LINKS.map((item) => (
             <li key={item.href} class="relative group cursor-pointer">
+=======
+  return (
+    <nav class="w-fit px-4 md:px-8 py-4 text-white z-50 relative">
+      <div id="desktop-menu" class="flex items-center justify-between">
+        <button
+          onClick={toggleMenu}
+          class="md:hidden focus:outline-none"
+          aria-label="Toggle Menu"
+        >
+          {isOpen ? <X size={24} /> : <Menu size={24} />}
+        </button>
+        <ul class="hidden md:flex gap-6 text-lg font-medium">
+          {LINKS.map((item) => (
+            <li class="relative group cursor-pointer">
+>>>>>>> a419378 (Overall structure of page)
               <a href={item.href} class="transition-colors duration-300">
                 {item.label}
               </a>
@@ -19,6 +35,7 @@ export default function Nav() {
             </li>
           ))}
         </ul>
+<<<<<<< HEAD
 
         <button
           onClick={toggleMenu}
@@ -45,6 +62,27 @@ export default function Nav() {
                 onClick={() => setIsOpen(false)}
               >
                 {l.label}
+=======
+      </div>
+
+      <div
+        id="mobile-menu"
+        class={`absolute right-0 top-full w-fit overflow-hidden transform transition-all duration-500 ease-in-out md:hidden ${
+          isOpen
+            ? "max-h-96 w-2xl opacity-100 translate-y-0"
+            : "max-h-0 opacity-0 -translate-y-2"
+        }`}
+      >
+        <ul class="flex flex-col gap-3 text-lg font-medium bg-gray-200 p-4 ">
+          {LINKS.map((item) => (
+            <li key={item.href}>
+              <a
+                href={item.href}
+                class="block px-2 py-1 hover:bg-blue-500 rounded"
+                onClick={() => setIsOpen(false)}
+              >
+                {item.label}
+>>>>>>> a419378 (Overall structure of page)
               </a>
             </li>
           ))}
