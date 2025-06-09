603ac774c047324f135a1046e148ba4e351f7ff3
Kajetan Nowak <kajtek2n@gmail.com>
2025-06-09 17:28:20 +0200

    Change styles of headers

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,10 +3,15 @@ import logo from "@/assets/logoipsum-379.svg";
 
 export default function Banner() {
   return (
-    <div>
-      <img src={logo} />
-      <p class="text-xs   text-white font-light">{SYSTEM_PSYCHOLOGY}</p>
-      <p class="text-xs  text-white font-bold">{WORKER_NAME}</p>
+    <div class="flex flex-row gap-3">
+      <img src={logo} alt="Logo" class="w-12 h-12" />
+
+      <div class="flex flex-col items-start sm:items-start">
+        <p class="text-xs lg:text-xl text-white font-light">
+          {SYSTEM_PSYCHOLOGY}
+        </p>
+        <p class="text-sm lg:text-3xl text-white font-bold">{WORKER_NAME}</p>
+      </div>
     </div>
   );
 }
