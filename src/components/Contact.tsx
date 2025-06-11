0e6e47d1e94f472567cff5d9861511bb716d2d2c
Kajetan Nowak <kajtek2n@gmail.com>
2025-06-11 17:29:42 +0200

    Populate with actual data, correct horizontal scrolling on mobile

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,7 +3,7 @@ Google map sources: https://www.embed-map.com/
 */
 import { Mail, MapPin, Phone } from "lucide-preact";
 import SectionHeader from "@/components/reusable/SectionHeader";
-import { CONTACT_HEADER } from "@/constants/contact";
+import { APPOINTMENT_INFO, CONTACT_HEADER } from "@/constants/contact";
 
 export default function ContactSection() {
   const phoneNumber = import.meta.env.VITE_PHONE_NUMBER;
@@ -11,42 +11,40 @@ export default function ContactSection() {
   const location = import.meta.env.VITE_LOCATION;
 
   return (
-    <section id="contact" data-aos="fade-right" className="py-16 px-4">
-      <div className="max-w-5xl mx-auto">
+    <section id="contact" data-aos="fade-right" class="py-16 px-4">
+      <div class="max-w-5xl mx-auto text-base">
         <SectionHeader title={CONTACT_HEADER} />
 
-        <div className="grid md:grid-cols-2 gap-8">
-          <div className="space-y-6 text-gray-700">
-            <div className="flex items-center gap-2">
-              <Phone className="text-green-500" />
+        <p class="mb-10 text-xs lg:text-xl">{APPOINTMENT_INFO}</p>
+        <div class="grid md:grid-cols-2 gap-8">
+          <div class="space-y-6 text-gray-700 text-base lg:text-xl">
+            <div class="flex items-center gap-2">
+              <Phone class="text-green-500" />
               <a
                 href={`tel:${phoneNumber}`}
-                className="text-blue-600 hover:underline"
+                class="text-blue-600 hover:underline"
               >
                 {phoneNumber}
               </a>
             </div>
 
-            <div className="flex items-center gap-2">
-              <Mail className="text-black" />
-              <a
-                href={`mailto:${email}`}
-                className="text-blue-600 hover:underline"
-              >
+            <div class="flex items-center gap-2">
+              <Mail class="text-black" />
+              <a href={`mailto:${email}`} class="text-blue-600 hover:underline">
                 {email}
               </a>
             </div>
 
-            <div className="flex items-center gap-2">
-              <MapPin className="text-red-600" />
+            <div class="flex items-center gap-2">
+              <MapPin class="text-red-600" />
               <p>{location}</p>
             </div>
           </div>
-          <div className="w-full h-[20rem] rounded-xl overflow-hidden shadow-xl">
+          <div class="w-full h-[25rem] rounded-xl overflow-hidden shadow-xl">
             <iframe
               title="Mapa lokalizacji"
-              src="https://www.google.com/maps/embed/v1/place?q=ujejskiego&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
-              className="w-full h-full border-0"
+              src="https://www.google.com/maps/embed/v1/place?q=pro+med.+nzoz.+gabinet+stomatologiczny&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+              class="w-full h-full border-0"
               allowFullScreen
               loading="lazy"
               referrerPolicy="no-referrer-when-downgrade"
