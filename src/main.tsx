3548ae4f2dc91ac91c4f9fa114647dcef6ffd735
Kajetan Nowak <kajtek2n@gmail.com>
2025-06-11 20:04:24 +0200

    Uninstall router

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,5 @@
 import { render } from "preact";
 import "./index.css";
 import { App } from "./app.tsx";
-import Router, { Route } from "preact-router";
-import PrivacyPolicy from "./components/PrivacyPolicy.tsx";
 
-const MainRouter = () => {
-  return (
-    <Router>
-      <Route component={App} path="/" />
-      <Route component={PrivacyPolicy} path="/privacy-policy" />
-    </Router>
-  );
-};
-render(<MainRouter />, document.getElementById("app")!);
+render(<App />, document.getElementById("app")!);
