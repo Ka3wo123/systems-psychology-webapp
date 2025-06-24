ebd57376191bfdb033ce67f3dbcd24688aaf0390
Kajetan Nowak <kajtek2n@gmail.com>
2025-06-24 09:25:39 +0200

    Add sitemap

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,5 +1,5 @@
 import { WORKER_NAME, SYSTEM_PSYCHOLOGY } from "@/constants/bio";
-import logo from "@/assets/logo.png";
+import logo from "@/assets/logo.webp";
 
 export default function Banner() {
   return (
--- src/constants/bio.ts
+++ src/constants/bio.ts
@@ -1,19 +1,21 @@
 export const HEADER_DESC = "Terapia systemowa – zaproszenie do wspólnej drogi";
 export const DESCRIPTION = `
-Jeśli doświadczasz trudności w relacjach – z partnerem, dzieckiem, rodzicami, czy innymi bliskimi osobami – terapia systemowa może być dla Ciebie wartościowym wsparciem.
+Jeśli doświadczasz trudności w relacjach – z partnerem, dzieckiem, rodzicami czy bliskimi – terapia systemowa może być dla Ciebie wartościowym wsparciem.
 
-Pracuję w podejściu systemowym, które patrzy na człowieka nie tylko jako jednostkę, ale jako część większej całości – rodziny, związku, grupy społecznej.
+🌿 <strong>Terapia rodzin i par</strong>, którą prowadzę, opiera się na podejściu systemowym. Patrzymy na człowieka nie tylko jako jednostkę, ale jako część większej całości – <strong>rodziny, związku, relacji społecznych</strong>.
 
-Jestem magistrem terapii systemowej, ukończyłam studia podyplomowe z tego kierunku. W mojej pracy to podejście pomaga mi spojrzeć szerzej – z empatią i zrozumieniem dla powiązań, które wpływają na Twoje codzienne życie i samopoczucie.
+👩‍⚕️ Jestem <strong>magistrem terapii systemowej</strong> z wykształceniem podyplomowym w tej dziedzinie. W mojej pracy terapeutycznej łączę <strong>empatię, zrozumienie i doświadczenie</strong> – by pomóc Ci spojrzeć szerzej na Twoją sytuację życiową.
 
-Zapraszam Cię na spotkanie, jeśli:
+Zapraszam na spotkanie, jeśli:
 
-– czujesz się zagubiony/a w relacjach,  
-– chcesz lepiej zrozumieć siebie i swoich bliskich,  
-– doświadczasz napięć rodzinnych lub konfliktów,  
-– jesteś w kryzysie – indywidualnie lub jako para, rodzina.
+✅ czujesz się zagubiony/a w relacjach,  
+✅ chcesz <strong>lepiej zrozumieć siebie i swoich bliskich</strong>,  
+✅ zmagasz się z napięciami rodzinnymi, konfliktami lub trudnościami w związku,  
+✅ przeżywasz <strong>kryzys emocjonalny<strong> — indywidualnie, w parze lub rodzinie.
 
-W bezpiecznej atmosferze, z uważnością na Twoją historię, wspólnie poszukamy rozwiązań, które będą dla Ciebie wspierające. Terapia systemowa może być pierwszym krokiem do zmiany, zrozumienia i ulgi.
+🧡 W bezpiecznej atmosferze, z uważnością na Twoją historię, wspólnie poszukamy rozwiązań, które będą dla Ciebie wspierające. Terapia systemowa może być pierwszym krokiem do <strong>zmiany, zrozumienia i ulgi<strong/>.
+
+<strong>Terapia systemowa</strong> to często pierwszy krok do zmiany, lepszego zrozumienia siebie oraz budowania zdrowszych relacji.
 `;
 
 export const SYSTEM_PSYCHOLOGY = "Psychologia systemowa";
